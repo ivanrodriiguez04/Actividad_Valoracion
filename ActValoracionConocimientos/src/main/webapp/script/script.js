fe6d83c7a429e8b47c5c97d216827d75b284043b
DESKTOP-R294T53\irm <ivanrodriiguez04@gmail.com>
2023-02-06 09:40:08 +0100

    6-2-23

---
+++ ActValoracionConocimientos/src/main/webapp/script/script.js
@@ -0,0 +1,22 @@
+function ejer1() {
+	var num1=10;
+	var num2=5;
+	var num3=20;
+	
+	if(num1==10){
+		var num2=15;
+		let num3=10;
+		document.write(num1+"\n");
+		document.write(num2+"\n");
+		document.write(num3+"\n");
+	}
+	
+		document.write(num1+"\n");
+		document.write(num2+"\n");
+		document.write(num3);
+}
+
+function Alerta(){
+    let num1 ="me llamo paco";
+    alert(num1);
+}
